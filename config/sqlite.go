15a685f49c3878c9fb265ed2d52c626894a06228
Rene Vallecillo <reneval@gmail.com>
2016-04-26 19:24:14 -0600

    fixing script

--- config/sqlite.go
+++ config/sqlite.go
@@ -3,7 +3,7 @@ package config
 //InitSqls has the init sql for each table
 var InitSqls = []string{
 	`
-    'CREATE TABLE IF NOT EXISTS 'user' (
+    CREATE TABLE IF NOT EXISTS 'user' (
 	'id'	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
 	'first_name'	TEXT,
 	'last_name'	TEXT,
@@ -13,7 +13,7 @@ var InitSqls = []string{
 	'created_at'	TEXT,
 	'updated_at'	TEXT
 );`,
-	`CREATE TABLE IF NOT EXISTS =  'attendance' (
+	`CREATE TABLE IF NOT EXISTS 'attendance' (
 	'id'	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
 	'checkin_time'	TEXT,
 	'checkout_time'	TEXT
